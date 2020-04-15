67709b536d75cf7bda3d1da20dae69d73ceda096
sotela88 <j-ms-27@hotmail.com>
2020-04-15 21:34:56 +0200

    Updates

--- functions/color-name.js
+++ functions/color-name.js
@@ -1,18 +1,46 @@
-/**GETTING COLOR RGB */
-const element = document.querySelector(".box1");
+/**BOX SELECTION */
+const box= document.querySelectorAll(".box")
+/**LOOP THE BOX */
+for (let i = 0; i <box.length  ; i++) {
+    
+    /**GETTING COLOR RGB */
+const element = box[i].firstElementChild;
 const style = getComputedStyle(element);
 const rgb = style["background-color"]
-let r= parseInt(rgb.split(",")[0].split("rgb(")[1]) ;
-let g= parseInt(rgb.split(",")[1]) ;
-let b= parseInt(rgb.split(",")[2].split(")")[0]) ;
-//console.log(r,g,b); 
+ r= parseInt(rgb.split(",")[0].split("rgb(")[1]) ;
+ g= parseInt(rgb.split(",")[1]) ;
+ b= parseInt(rgb.split(",")[2].split(")")[0]) ;
+console.log(r,g,b);    
+console.log(RGBToHex()); 
+
+
+
+/**COLORS */
+const n_match  = ntc.name(this.RGBToHex());
+n_rgb        = n_match[0]; // RGB value of closest match
+n_name       = n_match[1]; // Text string: Color name
+console.log(n_name); 
+
+/**COLOR NAMES */ 
+const name = document.querySelectorAll(".name");
+
+/**HEX CODE */
+const code= document.querySelectorAll(".code");
+
+/**INSERTING THE HEX CODE AND COLOR NAME */
+name[i].innerHTML=n_name;  
+code[i].innerHTML= n_rgb ; 
+
+} 
+
+
 
 /**RGB TO HEX */
 function RGBToHex() {
     r = r.toString(16);
     g = g.toString(16);
     b = b.toString(16);
-   //console.log(r);
+  // console.log(r); 
     if (r.length == 1)
       r = "0" + r;
     if (g.length == 1)
@@ -25,27 +53,5 @@ function RGBToHex() {
   //console.log(RGBToHex());  
   
 
-/**COLORS */
-const n_match  = ntc.name(`${RGBToHex()}`);
-n_rgb        = n_match[0]; // RGB value of closest match
-n_name       = n_match[1]; // Text string: Color name
-n_exactmatch = n_match[2]; // True if exact color match
 
-/**COLOR NAMES */
-const color1 = document.querySelector("#color-name1");
-const color2 = document.querySelector("#color-name2");
-const color3 = document.querySelector("#color-name3");
-const color4 = document.querySelector("#color-name4");
-//console.log(color1);
-/**HEX CODE */
-const codeColor1 = document.querySelector("#color-code1");
-const codeColor2 = document.querySelector("#color-code2");
-const codeColor3 = document.querySelector("#color-code3");
-const codeColor4 = document.querySelector("#color-code4");
-
-
-
-/**INSERTING THE HEX CODE AND COLOR NAME */
-color1.innerHTML=n_match[1];  
-codeColor1.innerHTML= n_match[0]
 
